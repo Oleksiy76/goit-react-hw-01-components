c04596175e45f4c8ab34eb26ab1a3b1ea6b8b155
Oleksiy76 <alrainbow76@gmail.com>
2023-06-20 13:09:34 +0300

    added task 4

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -36,17 +36,3 @@ Profile.propTypes = {
   avatar: PropTypes.string.isRequired,
   stats: PropTypes.object.isRequired,
 };
-
-// Profile.propTypes = {
-//   username: PropTypes.string.isRequired,
-//   tag: PropTypes.string.isRequired,
-//   location: PropTypes.string.isRequired,
-//   avatar: PropTypes.string.isRequired,
-//   stats: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       followers: PropTypes.number.isRequired,
-//       views: PropTypes.number.isRequired,
-//       likes: PropTypes.number.isRequired,
-//     })
-//   ),
-// };
